9ee9809f3bf872a9472eec79b57050f4c8674aa8
Jacob <callaghanjacob@gmail.com>
2022-03-04 15:44:49 +0000

    Song class added

---
+++ classes/song.py
@@ -0,0 +1,4 @@
+class Song:
+    def __init__(self, name, duration):
+        self.name = name
+        self.duration = duration
--- tests/song_test.py
+++ tests/song_test.py
@@ -3,7 +3,10 @@ from classes.song import Song
 
 class TestSong(unittest.TestCase):
     def setUp(self):
-        self.starlight_song = Song("Starlight")
+        self.starlight_song = Song("Starlight", 200)
 
     def test_song_has_name(self):
         self.assertEqual("Starlight", self.starlight_song.name)
+
+    def test_song_has_duration(self):
+        self.assertEqual(200, self.starlight_song.duration)

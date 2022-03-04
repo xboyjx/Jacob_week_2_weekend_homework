dec95fc2d6553b01b9e47e368324e9711834ec47
Jacob <callaghanjacob@gmail.com>
2022-03-04 16:05:49 +0000

    test for adding song to room added

--- tests/room_test.py
+++ tests/room_test.py
@@ -1,11 +1,13 @@
 import unittest
 from classes.room import Room
 from classes.guest import Guest
+from classes.song import Song
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
         self.main_room = Room("Main Room")
         self.new_guest_1 = Guest ("Jacob")
+        self.starlight_song = Song("Starlight", 200)
 
     def test_room_has_name(self):
         self.assertEqual("Main Room", self.main_room.name)
@@ -25,3 +27,7 @@ class TestRoom(unittest.TestCase):
         self.assertEqual(1, len(self.main_room.guest_list))
         self.main_room.remove_guest(self.new_guest_1)
         self.assertEqual(0, len(self.main_room.guest_list))
+
+    def test_room_can_add_song(self):
+        self.main_room.add_song(self.starlight_song)
+        self.assertEqual(1, self.main_room.song_list)

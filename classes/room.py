70484e1d1f3934a886bfaf2a268e7f690dc902f1
Jacob <callaghanjacob@gmail.com>
2022-03-04 15:51:26 +0000

    add_guest fuctionality added

--- classes/room.py
+++ classes/room.py
@@ -3,4 +3,6 @@ class Room:
         self.name = name
         self.guest_list = []
         self.song_list = []
-        
+
+    def add_guest(self, new_guest):
+        self.guest_list.append(new_guest)
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,9 +1,11 @@
 import unittest
 from classes.room import Room
+from classes.guest import Guest
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
         self.main_room = Room("Main Room")
+        self.new_guest_1 = Guest ("Jacob")
 
     def test_room_has_name(self):
         self.assertEqual("Main Room", self.main_room.name)
@@ -13,3 +15,7 @@ class TestRoom(unittest.TestCase):
 
     def test_room_has_song_list(self):
         self.assertEqual(0, len(self.main_room.song_list))
+
+    def test_room_can_add_guest(self):
+        self.main_room.add_guest(self.new_guest_1)
+        self.assertEqual(1, len(self.main_room.guest_list))
